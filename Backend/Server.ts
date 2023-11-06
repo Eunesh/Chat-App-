229ef8a1b1c92e0bb95232cc481725915013e1c3
Srijana Shahi <srijanashahi@Srijanas-MacBook-Pro.local>
2023-11-06 23:16:19 +0545

    Setup Completed

---
+++ Backend/Server.ts
@@ -0,0 +1,10 @@
+import { app } from "./index";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env" });
+
+const PORT: string | undefined = process.env.PORT; // FOR PORT
+
+app.listen(PORT, () => {
+  console.log(`Server is listining on port ${PORT}`);
+});
